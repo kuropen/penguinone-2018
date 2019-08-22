8a6fdb71c28b8aa9cc8f5fcb8f9db6f8ee68f43e
Hirochika Yuda <hyuda@kuropen.me>
2019-08-23 00:26:38 +0900

    fix: Fix test and lint errors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Container, Image, Menu, Segment, Responsive, Icon } from 'semantic-ui-react';
+// import { Container, Image, Menu, Segment, Responsive, Icon } from 'semantic-ui-react';
+import { Container, Image, Menu, Segment } from 'semantic-ui-react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import logo from './penguin.png';
 import Top from './Top/Top.js';
